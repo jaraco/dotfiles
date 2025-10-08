668ba0558198c91d55c93ffb6fea36e9652a02ab
Jason R. Coombs <jaraco@jaraco.com>
2025-10-08 10:07:44 -0400

    Update settings for use at Microsoft

--- .hyper.js
+++ .hyper.js
@@ -100,7 +100,7 @@ module.exports = {
     //
     // PowerShell on Windows
     // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
-    shell: '',
+    shell: 'C:\\Users\\jaraco\\.local\\bin\\xonsh.exe',
 
     // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
     // by default `['--login']` will be used
